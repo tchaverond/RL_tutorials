23ac33240c4c4b5a86518ab1c43d53aaa1dc033a
tchaverond <thomas.chaverondier@gmail.com>
2019-12-22 20:34:50 +0100

    Added wind to grid world

--- policy_improvement_with_ipe.py
+++ policy_improvement_with_ipe.py
@@ -30,7 +30,7 @@ class PolicyOptimizer:
         self.state_values = {s:0 for s in self.env.all_states()}
 
 
-    def update_state_value_function(self, policy):
+    def update_state_value_function(self, policy, wind, wind_force=0.5):
 
         deltas = [1]
         while max(deltas) > self.epsilon:
@@ -39,22 +39,34 @@ class PolicyOptimizer:
                 self.env.set_state(s)
                 old_value = self.state_values[s]
                 self.state_values[s] = 0
-                if policy == 'random':
-                    p_a = 1 / len(self.env.actions[s])
-                    for a in self.env.actions[s]:
-                        reward = self.env.move(a)
-                        next_state = self.env.current_state()
-                        self.state_values[s] += p_a * (reward + self.discount * self.state_values[next_state])
-                        self.env.undo_move(a)
-                elif policy == 'self':
-                    reward = self.env.move(self.policy[s])
+                for a in self.env.actions[s]:
+                    if policy == 'random':
+                        p_a = 1 / len(self.env.actions[s])
+                    elif policy == 'self':
+                        if wind == 'random':
+                            if self.policy[s] == a:
+                                p_a = wind_force
+                            else:
+                                p_a = wind_force * (1 / (len(self.env.actions[s]))-1)
+                        elif wind == 'right':
+                            if a == 'R' and a in self.env.actions[s]:
+                                p_a = wind_force
+                            else:
+                                p_a = 0
+                            if self.policy[s] == a:
+                                p_a += (1 - wind_force)
+                        else:
+                            p_a = int(self.policy[s] == a)
+                    if p_a == 0 :
+                        continue
+                    reward = self.env.move(a)
                     next_state = self.env.current_state()
-                    self.state_values[s] = reward + self.discount * self.state_values[next_state]
-                    self.env.undo_move(self.policy[s])
+                    self.state_values[s] += p_a * (reward + self.discount * self.state_values[next_state])
+                    self.env.undo_move(a)
                 deltas[i] = np.abs(self.state_values[s] - old_value)
 
 
-    def improve_policy(self):
+    def improve_policy(self, **kwargs):
 
         policy_changed = True
         i = 0
@@ -73,7 +85,7 @@ class PolicyOptimizer:
                 if best_action != self.policy[s]:
                     self.policy[s] = best_action
                     policy_changed = True
-            self.update_state_value_function(policy='self')
+            self.update_state_value_function(policy='self', **kwargs)
 
             i += 1
             if i%1 == 0:
@@ -88,5 +100,11 @@ if __name__ == "__main__":
     optimizer = PolicyOptimizer(environment=grid_world.negative_grid())
     print_policy(optimizer.policy, optimizer.env)
     print_values(optimizer.state_values, optimizer.env)
-    optimizer.improve_policy()
+    optimizer.improve_policy(wind=None)
+
+    # Windy Gridworld: each action has a 50% chance to fail, another action (chosen at random) is performed instead
+    optimizer = PolicyOptimizer(environment=grid_world.negative_grid())
+    print_policy(optimizer.policy, optimizer.env)
+    print_values(optimizer.state_values, optimizer.env)
+    optimizer.improve_policy(wind='right', wind_force=0.26)   # .25 is the threshold to switch optimal agency
 
