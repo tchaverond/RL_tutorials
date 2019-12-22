7b5d1b965c252b0aefc94b43c9eb5de35720f340
tchaverond <thomas.chaverondier@gmail.com>
2019-12-22 23:11:33 +0100

    Add value iteration option (faster way to solve control problem)

--- policy_improvement_with_ipe.py
+++ policy_improvement_with_ipe.py
@@ -21,8 +21,8 @@ class PolicyOptimizer:
         self.terminal_states = [s for s in self.env.all_states() if self.env.is_terminal(s)]
         self.state_values = None
         self.initialize_state_value_function()
-        # starting with 'always-up' policy
-        self.policy = {k: 'U' for k in self.action_states}
+        # starting with random policy
+        self.policy = {k: np.random.choice(self.env.actions[k], 1)[0] for k in self.action_states}
 
 
     def initialize_state_value_function(self):
@@ -94,17 +94,59 @@ class PolicyOptimizer:
                 print_values(self.state_values, self.env)
 
 
+    def perform_value_iteration(self, wind, wind_force=0.5):
+
+        deltas = [1]
+        t = 0
+        while max(deltas) > self.epsilon:
+            deltas = [None] * len(self.action_states)
+            for i, s in enumerate(self.action_states):
+                best_action_value = -1
+                self.env.set_state(s)
+                old_value = self.state_values[s]
+                self.state_values[s] = 0
+                for a in self.env.actions[s]:
+                    if wind == 'random':
+                        if self.policy[s] == a:
+                            p_a = wind_force
+                        else:
+                            p_a = wind_force * (1 / (len(self.env.actions[s])) - 1)
+                    elif wind == 'right':
+                        if a == 'R' and a in self.env.actions[s]:
+                            p_a = wind_force
+                        else:
+                            p_a = 0
+                        if self.policy[s] == a:
+                            p_a += (1 - wind_force)
+                    else:
+                        p_a = int(self.policy[s] == a)
+                    reward = self.env.move(a)
+                    next_state = self.env.current_state()
+                    action_value = reward + self.discount * self.state_values[next_state]
+                    if action_value > best_action_value:
+                        best_action_value = action_value
+                        best_action = a
+                    self.state_values[s] += p_a * action_value
+                    self.env.undo_move(a)
+                self.policy[s] = best_action
+                deltas[i] = np.abs(self.state_values[s] - old_value)
+
+            t += 1
+            print("Number of iterations: ", t)
+            print_policy(self.policy, self.env)
+            print_values(self.state_values, self.env)
+
 
 if __name__ == "__main__":
 
     optimizer = PolicyOptimizer(environment=grid_world.negative_grid())
     print_policy(optimizer.policy, optimizer.env)
     print_values(optimizer.state_values, optimizer.env)
-    optimizer.improve_policy(wind=None)
+    optimizer.perform_value_iteration(wind=None)
 
     # Windy Gridworld: each action has a 50% chance to fail, another action (chosen at random) is performed instead
     optimizer = PolicyOptimizer(environment=grid_world.negative_grid())
     print_policy(optimizer.policy, optimizer.env)
     print_values(optimizer.state_values, optimizer.env)
-    optimizer.improve_policy(wind='right', wind_force=0.26)   # .25 is the threshold to switch optimal agency
+    optimizer.perform_value_iteration(wind='right', wind_force=0.26)   # .25 is the threshold to switch optimal agency
 
